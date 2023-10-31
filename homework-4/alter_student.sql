5690572437bc1481fbb6170e9d736b69222a9ba6
dmitriyalushkin <pantific37@mail.ru>
2023-10-31 10:03:17 +0300

    Add changes alter_northwind.sql, alter_student.sql.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,30 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student
+(
+student_id serial,
+first_name varchar,
+last_name varchar,
+birthday date,
+phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student(first_name, last_name, birth_date, phone) VALUES
+('Dmitriy', 'Hurner', '1997-02-03', '(958)7412323'),
+('Anna', 'Norton', '1995-05-09', '(923)7484523'),
+('Vladimir', 'Utkin', '1998-10-07', '(947)6332329');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
